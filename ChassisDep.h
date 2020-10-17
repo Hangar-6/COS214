7329d8960693278ed2da93d18bab18bf1198ea7f
Aztrixs <72404660+Aztrixs@users.noreply.github.com>
2020-10-17 17:09:29 +0200

    New .h and .cpp files

---
+++ ChassisDep.h
@@ -0,0 +1,14 @@
+//
+// Created by Luca Azmanov on 2020/10/17.
+//
+
+#ifndef COS214_CHASSISDEP_H
+#define COS214_CHASSISDEP_H
+
+
+class ChassisDep {
+
+};
+
+
+#endif //COS214_CHASSISDEP_H
---
+++ Stratergist.h
@@ -0,0 +1,14 @@
+//
+// Created by Luca Azmanov on 2020/10/17.
+//
+
+#ifndef COS214_STRATERGIST_H
+#define COS214_STRATERGIST_H
+
+
+class Stratergist {
+
+};
+
+
+#endif //COS214_STRATERGIST_H
