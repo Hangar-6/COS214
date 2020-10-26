ea838b8649c54f2d3d1e7107cca6454c57e59610
v24Zer0 <vinbesse@gmail.com>
2020-10-26 21:03:48 +0200

    Added functions to Team.h

--- Team.h
+++ Team.h
@@ -9,7 +9,18 @@ using namespace std;
 #include "Strategist.h"
 #include "Logistics.h"
 #include "Testing.h"
+#include "BaseCar.h"
 #include <string>
+
+struct Cars {
+    Car* car1;
+    Car* car2;
+};
+
+struct Engineers {
+    Engineering* current;
+    Engineering* next;
+};
 
 class Team {
     private:
@@ -29,8 +40,12 @@ class Team {
         Logistics* logistics;
 
     public:
+        Team();
         ~Team();
         void addPoints(int d1Points, int d2Points);
+        void runTest(Testing* testing, TestType type);
+        Cars getCars();
+        Engineers getEngineers();
 };
 
 #endif
